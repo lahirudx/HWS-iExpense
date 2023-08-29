3f38f78afa229f8670fe584e96f67247bbb24813
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-08-29 22:44:18 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Lahiru Amarasooriya on 2023-08-29.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
